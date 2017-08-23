36ef70ed015b46b9a410708c7665af4ba51fba68
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-08-23 13:46:31 +0200

    Fix getting extras from dict

--- ckanext/certificates/helpers.py
+++ ckanext/certificates/helpers.py
@@ -5,7 +5,7 @@ def has_certificate(pkg):
     Can be used in the template to determine if the package has a
     certificate. Returns a boolean.
     """
-    return 'odi-certificate' in pkg.extras and get_certificate_data(pkg) is not None
+    return 'odi-certificate' in pkg.get('extras') and get_certificate_data(pkg) is not None
 
 def get_certificate_data(pkg):
     """
@@ -13,6 +13,6 @@ def get_certificate_data(pkg):
     given package
     """
     try:
-        return json.loads(pkg.extras.get('odi-certificate'))
+        return json.loads(pkg.get('extras').get('odi-certificate'))
     except ValueError:
         return None
