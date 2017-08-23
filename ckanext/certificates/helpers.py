218b36507015c7f7239ea0554265b59fef3ca086
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2017-08-23 14:08:55 +0200

    Fix helper methods

--- ckanext/certificates/helpers.py
+++ ckanext/certificates/helpers.py
@@ -1,18 +1,33 @@
 import json
 
+
 def has_certificate(pkg):
     """
     Can be used in the template to determine if the package has a
     certificate. Returns a boolean.
     """
-    return 'odi-certificate' in pkg.get('extras') and get_certificate_data(pkg) is not None
+
+    extras = pkg.get('extras')
+
+    if extras:
+        for extra in extras:
+            if extra.get('key') == 'odi-certificate' and\
+               get_certificate_data(pkg) is not None:
+                    return True
+
 
 def get_certificate_data(pkg):
     """
     Returns the dictionary containing information about the certificate for the
     given package
     """
-    try:
-        return json.loads(pkg.get('extras').get('odi-certificate'))
-    except ValueError:
-        return None
+
+    extras = pkg.get('extras')
+
+    if extras:
+        for extra in extras:
+            if extra.get('key') == 'odi-certificate':
+                try:
+                    return json.loads(extra.get('value'))
+                except ValueError:
+                    return None
